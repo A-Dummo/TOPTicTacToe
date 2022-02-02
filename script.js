cd41742445ca6d0a3bf1d7abee4bbde6afc11a6f
A-Dummo <2014adamw@gmail.com>
2022-02-02 00:43:04 -0800

    Add function computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(1)
+function computerPlay() {
+    const randomResult = Math.floor(Math.random() * 3) + 1; // 1-3
+    switch(randomResult) {
+        case 1:
+            return "Rock";
+        
+        case 2:
+            return "Paper";
+        
+        case 3:
+            return "Scissors";
+        
+        default:
+            return "None";
+    }
+}
